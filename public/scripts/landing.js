8138068822e5df81c9e66ae9a41cfb37e31c491a
flame539275 <18711194+flame539275@users.noreply.github.com>
2018-07-17 12:00:54 -0700

    Pre-loaded game keys can no longer be already taken.

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -4,19 +4,35 @@ class Landing extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = { gameId: '' }
+    this.state = { gameId: '',
+                   proposedID: '',
+                   invalidID: ''}
     
-    var txtFile = "../../wordlist.txt"          //maybe filepath needs fixing
-    var file = new File(txtFile)
-    file.open("r"); // open file with read access
-    var wordArray = []
-    var i = 0
+    txtFile = "../../wordlist.txt"          //maybe filepath needs fixing
+    file = new File(txtFile)
+    file.open("r") // open file with read access
+    wordArray = []
+    i = 0
     while (!file.eof) {
       // store each word in the array
       wordArray[i] = file.readln()
     }
     file.close()
-    this.setState({ gameId: wordArray[Math.floor(Math.random() * wordArray.length)] })  
+    do{
+      this.state.proposedID = wordArray[Math.floor(Math.random() * wordArray.length)]
+      $.post({
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        data: JSON.stringify({ game_id: proposedID }),
+        url: `http://${DOMAIN}/new_game`,
+        success: this.onCreateGame
+      }).fail(function(data) {
+        const response = data.responseJSON
+        if (response.error === "Game with given game_id already exists.") {  
+          this.state.invalidID = true
+        }
+    }while(this.state.invalidID)
+    this.setState({ gameId: this.state.proposedID})  
 
     this.createGame = this.createGame.bind(this)
     this.handleChange = this.handleChange.bind(this)
